701627b938a2d80c95882fc2b6c53726ae381fc9
thicchead <metehan.karakoruk@gmail.com>
2022-08-18 18:08:28 +0200

    1a gedaan

---
+++ h1/h1.1.js
@@ -0,0 +1,37 @@
+let waarde = 1;
+/* console.log(waarde) */
+
+let aantal;
+aantal = 5;
+/* console.log(aantal) */
+
+// const con;           --> declaratie en initialisatie moeten in 1 keer
+const con = 5;
+// con = 3;             --> niet veranderbaar
+/* console.log(con) */
+
+let nummer = 17;
+/* console.log(nummer / 2);
+console.log(nummer % 2); */
+
+let getal = 10;
+/* console.log(`${getal} + 1 = ${getal + 1}`);
+console.log(`getal * 20 = ${getal * 20} :o`);
+console.log(`50 delen door 20 is gelijk aan ${50 / 20}`); */
+
+let string = "Metehan";
+let eersteLetter = string[0];
+/* console.log(string[5]);
+console.log(eersteLetter);
+console.log(typeof eersteLetter); */
+
+let naam = "Metehan";
+let naam1 = new String("Metehan");
+/* console.log(naam.toUpperCase());
+console.log(typeof naam);
+console.log(typeof naam1); */
+
+let mijnLeeftijd = 18;
+let keuze = mijnLeeftijd >= 18 ? "volwassenen" : "minderjarig";
+console.log(keuze)
+
