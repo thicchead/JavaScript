eaef813dd7d475705c41b2d9ccef536dacf1f621
thicchead <metehan.karakoruk@gmail.com>
2022-08-19 18:14:25 +0200

    fuck unshift

--- h4/oef/sixtest.js
+++ h4/oef/sixtest.js
@@ -1,6 +1,7 @@
 let array = [1, 2, 3];
 
 console.log(array.length);
-
-array[0] = array.unshift(array.length)
+let num = array.length - 1;
+array[0] = array.unshift(array.length-1)
 console.log(array)
+console.log(num)
